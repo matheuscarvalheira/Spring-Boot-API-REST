a16ee7a00fd6e056011f733054d71a7d66a41c07
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-21 16:32:56 -0300

    modifying the imports

--- src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.alura.forum.modelo.Topico;
-import br.com.alura.forum.repository.TopicoRepository;
+import br.com.alura.forum.modelo.repository.TopicoRepository;
 
 public class AtualizacaoTopicoForm {
 
