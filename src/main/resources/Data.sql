7e69fe0e1a71d577c9caf4fa7f52908edb8724d5
matheus carvalheira <matheuscarvalheiraf@gmail.com>
2022-02-22 10:33:36 -0300

    putting an encrypted password

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$vd/cDO6V5fed6lgQA3NDvuS0ZzQQQnkVGI7MGEvRaAwD.atl5U2UK');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
